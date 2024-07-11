fb33a3619c851e091d2ccfc65010e52ce5f0c692
pentimetromx <juanosvaldo7@yahoo.es>
2024-07-11 01:03:03 -0500

    ok

--- module2.js
+++ module2.js
@@ -266,7 +266,7 @@ document.addEventListener('keydown', function(event) {
 // SECCION EXTRAER DATOS A  ELEMENTOS DEL DOM
 function Geometria() {
   console.clear();
-  var contiBoton = document.getElementById('cont-arriba-freno') 
+  var contiBoton = document.getElementById('conti-boton-rodilleria') 
   var rect = contiBoton.getBoundingClientRect();
   var topPosition = rect.top;
   var leftPosition = rect.left;
